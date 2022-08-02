49a542971ebc7a7eb214bcb41415191c66bad305
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-02 12:22:08 +0300

    Добавил функцию проверки наличия книги

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import requests
 import os
+from dotenv import load_dotenv
 
 
-def download_book(url, pach, filename):
-    response = requests.get(url)
-    response.raise_for_status()
+def download_book(book, pach, filename):
     with open(f'{pach}{filename}', 'wb') as file:
-        file.write(response.content)
+        file.write(book)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.HTTPError('Ответ пришёл с главной, а не с запрошенной страницы')
 
 
 def main():
-    os.makedirs('books/', exist_ok=True)
-    url = 'https://tululu.org/txt.php?id=32168'
-    pach = 'books/'
-    filename = 'книга.txt'
-    download_book(url, pach, filename)
+
+    load_dotenv()
+    book_folder=os.getenv('BOOK_FOLDER', 'books/')
+    os.makedirs(book_folder, exist_ok=True)
+
+    for number in range(1, 11):
+        book_download_link = f'https://tululu.org/txt.php?id={number}'
+        response = requests.get(book_download_link)
+        response.raise_for_status()
+        filename = f'книга{number}.txt'
+        try:
+            check_for_redirect(response)
+        except requests.HTTPError:
+            continue
+        book = response.content
+        download_book(book, book_folder, filename)
 
 
 if __name__ == '__main__':
