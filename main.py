e17d9841718a871ad45d09352591b36d5bbda4a0
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-02 14:13:52 +0300

    Получение названия через функцию

--- main.py
+++ main.py
@@ -19,13 +19,26 @@ def check_for_redirect(response):
         raise requests.HTTPError('Ответ пришёл с главной, а не с запрошенной страницы')
 
 
+def parse_book_page(url, book_id):
+    response = requests.get(url)
+    response.raise_for_status()
+
+    soup = BeautifulSoup(response.text, 'lxml')
+    title_tag = soup.find('h1')
+    book_title = title_tag.text.split('::')[0].strip()
+    book_author = title_tag.text.split('::')[1].strip()
+    full_book_title = f'{book_id}. {book_title}'
+
+    return full_book_title
+
+
 def main():
 
     load_dotenv()
     book_folder=os.getenv('BOOK_FOLDER', 'books/')
     os.makedirs(book_folder, exist_ok=True)
 
-    for number in range(1, 11):
+    for number in range(1, 2):
         book_download_link = f'https://tululu.org/txt.php?id={number}'
         response = requests.get(book_download_link)
         response.raise_for_status()
@@ -35,16 +48,10 @@ def main():
         except requests.HTTPError:
             continue
 
-        book_link = f'https://tululu.org/read{number}'
-        response = requests.get(book_link)
-        response.raise_for_status()
-        
-        soup = BeautifulSoup(response.text, 'lxml')
-        title_tag = soup.find('h1')
-        book_title = title_tag.text.split('::')[0].strip()
-        full_book_title = f'{number}. {book_title}'
-        
-        download_txt(book_download_link, full_book_title, folder=book_folder)
+        book_link = f'https://tululu.org/b{number}/'
+        book_title = parse_book_page(book_link, number)
+
+        download_txt(book_download_link, book_title, folder=book_folder)
 
 
 if __name__ == '__main__':
