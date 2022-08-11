fc15d87b1dd4c29fded7c72e5363009ddd2540cf
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-11 10:41:32 +0300

    parse books genres

--- main.py
+++ main.py
@@ -33,18 +33,27 @@ def parse_book_page(url, book_id):
     response.raise_for_status()
 
     soup = BeautifulSoup(response.text, 'lxml')
+    
     book_cover_relative_link = soup.find('div', class_='bookimage').find('img')['src']
     book_cover_link = urljoin(url, book_cover_relative_link)
+    
     title_tag = soup.find('h1')
     book_title = title_tag.text.split('::')[0].strip()
     book_author = title_tag.text.split('::')[1].strip()
     full_book_title = f'{book_id}. {book_title}'
+    
     comments_tag = soup.find_all('div', class_='texts')
+    
     book_comments = []
     for book_comment in comments_tag:
         book_comments.append(book_comment.find('span').text)
+    genres_tag = soup.find('span', class_='d_book').find_all('a')
+    
+    book_genres = []
+    for book_genre in genres_tag:
+        book_genres.append(book_genre.text)
         
-    return full_book_title, book_cover_link, book_comments
+    return full_book_title, book_cover_link, book_comments, book_genres
 
 
 def main():
@@ -65,7 +74,7 @@ def main():
             continue
 
         book_link = f'https://tululu.org/b{number}/'
-        book_title, book_cover_link, book_comments = parse_book_page(book_link, number)
+        book_title, book_cover_link, book_comments, books_genres = parse_book_page(book_link, number)
         
         download_txt(book_download_link, book_title, folder=book_folder)
         download_image(book_cover_link, number)
