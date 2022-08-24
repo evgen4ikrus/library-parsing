8878f7c53450f3169a0e7cc1c7c67d045133675c
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-24 10:52:45 +0300

    add ConnectionError in case of internet loss

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -3,10 +3,11 @@ import requests
 from parse_tululu import get_html_content, download_txt, parse_book_page, download_image
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
-from requests.exceptions import HTTPError
+from requests.exceptions import HTTPError, ConnectionError
 from pathlib import Path
 import json
 import re
+from time import sleep
 
 
 def save_json_file(book):
@@ -50,8 +51,6 @@ def main():
 
     while start_page < end_page:
 
-        success_iteration = True
-
         try:
             url = f'https://tululu.org/l55/{start_page}'
             response = requests.get(url)
@@ -62,25 +61,39 @@ def main():
             book_pages = soup.select(book_selector)
 
         except HTTPError:
-            print(f'Страница {start_page} не найдена, скачивание книг закончено')
+            print(f'Страница {start_page} не найдена')
             break
-        
+
+        except ConnectionError:
+            print('Связь с интернетом потеряна, ожидание подключения...')
+            sleep(5)
+            continue
+
         for book_page in book_pages:
+            success_iteration = False
+            while success_iteration == False:
 
-            try:
-                book_relative_link = book_page['href']
-                book_id = re.search(r'\d+', book_relative_link).group()
-                book_link = urljoin(url, book_relative_link)
-                html_content = get_html_content(book_link)
-                book_download_link = f'https://tululu.org/txt.php?id={book_id}'
-                book = parse_book_page(book_link, html_content, book_id)
-                download_txt(book_download_link, book['title'], books_path)
-                # download_image(book['cover_link'], book['title'], covers_pach)
-                book_catalog.append(book)
+                try:
+                    book_relative_link = book_page['href']
+                    book_id = re.search(r'\d+', book_relative_link).group()
+                    book_link = urljoin(url, book_relative_link)
+                    html_content = get_html_content(book_link)
+                    book_download_link = f'https://tululu.org/txt.php?id={book_id}'
+                    book = parse_book_page(book_link, html_content, book_id)
+                    download_txt(book_download_link, book['title'], books_path)
+                    download_image(book['cover_link'], book['title'], covers_pach)
+                    book_catalog.append(book)
 
-            except HTTPError:
-                print(f'Книги с id={book_id} нет на сайте')
-                continue
+                except HTTPError:
+                    print(f'Книги с id={book_id} нет на сайте')
+                    success_iteration = True
+
+                except ConnectionError:
+                    print('Связь с интернетом потеряна, ожидание подключения.')
+                    sleep(5)
+                    continue
+
+                success_iteration = True
 
         start_page += 1
 
