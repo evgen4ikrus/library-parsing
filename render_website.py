d4b780f1c7c91bd2d5df8ca08f468581da2ca94e
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-26 10:55:17 +0300

    integrate livereload

--- render_website.py
+++ render_website.py
@@ -2,23 +2,30 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import json
+from livereload import Server
+
 
 env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
 )
 
-template = env.get_template('template.html')
 
-
-with open("my_library/book_catalog.json", "r", encoding='utf8') as my_file:
-    book_catalog_json = my_file.read()
+def rebuild():
     
-book_catalog = json.loads(book_catalog_json)
-rendered_page = template.render(book_catalog=book_catalog)
+    template = env.get_template('template.html')
+    with open("my_library/book_catalog.json", "r", encoding='utf8') as my_file:
+        book_catalog_json = my_file.read()
+        
+    book_catalog = json.loads(book_catalog_json)
+    rendered_page = template.render(book_catalog=book_catalog)
 
-with open('index.html', 'w', encoding="utf8") as file:
-    file.write(rendered_page)
+    with open('index.html', 'w', encoding="utf8") as file:
+        file.write(rendered_page)
 
-server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
-server.serve_forever()
+
+if __name__ == '__main__':
+    
+    server = Server()
+    server.watch('template.html', rebuild)
+    server.serve(root='.')
