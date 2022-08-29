5f64de5193b32a12699af2c8ef9deb8cb8d9b076
evgen4ikrus <67272160+evgen4ikrus@users.noreply.github.com>
2022-08-29 21:11:32 +0300

    add paginator

--- render_website.py
+++ render_website.py
@@ -14,20 +14,25 @@ env = Environment(
 def rebuild():
 
     template = env.get_template('template.html')
+    books_per_page = 10
+    
     with open("my_library/book_catalog.json", "r", encoding='utf8') as my_file:
         book_catalog_json = my_file.read()
 
     folder_path = 'pages'
     os.makedirs(folder_path, exist_ok=True)
     books = json.loads(book_catalog_json)
-    books_on_pages  = list(chunked(books, 4))
+    books_on_pages  = list(chunked(books, books_per_page))
 
     for page_num, books_on_page in enumerate(books_on_pages, start=1):
+        number_pages = len(books_on_pages)
         file_name = f'index{page_num}.html'
         file_path = os.path.join(folder_path, file_name)
         
         rendered_page = template.render(
             books_on_page=books_on_page,
+            number_pages=number_pages,
+            page_num=page_num,
         )
 
         with open(file_path, 'w', encoding="utf8") as file:
